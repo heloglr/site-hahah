372b112099c56e8cfc2689ee713a37b4cb3b526f
heloglr <heloisagemmi586@gmail.com>
2025-09-09 11:41:44 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.image("https://wallpapers.com/images/hd/turtle-close-up-dr4axqrrae7it9m0.jpg")
+st.title ("my site😜")
+st.subheader("criadora com st 🐢")
+
+st.markdown(""" 
+# titulo/n
+*italico* /n
+**negrito**
+""")
+
